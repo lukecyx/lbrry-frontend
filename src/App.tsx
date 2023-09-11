334c2b3cb9ecfd2b48c167532ef4665c6422b1d5
Luke Holmes <lukemartynholmes@gmail.com>
2023-09-11 18:02:54 +0100

    feat: Add prettier.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import "./App.css";
-
+import BookList from "./components/books/BookList";
 
 function App() {
     return (
-        <>
+        <div className="max-w-screen-xl mx-auto text-center p-2">
             <h1 className="text-3xl font-bold">Hello world!</h1>
-        </>
+            <BookList />
+        </div>
     );
 }
 
